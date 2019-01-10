2b799a68a5d462b21ea8e153f706258c329e243d
Bryan <bryan.curchod@gmail.com>
2019-01-10 14:06:25 +0100

    Popular film request done

--- src/app/popular-page/popular-page.component.ts
+++ src/app/popular-page/popular-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-popular-page',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PopularPageComponent implements OnInit {
 
-  constructor() { }
+  imageURL: string = "https://image.tmdb.org/t/p/w500/";
+  noPage: number = 1;
+
+
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.executeRequest
+  }
+
+  /**
+   * Execute the get request to gather the popular films
+   */
+  executeRequest(): void {
+    const requestURL = environment.API_URL + "3/movie/popular?page=" + this.noPage + "&api_key=" + environment.API_KEY;
+    // lancement de la requête
+    this.http.get(requestURL).toPromise().then(
+      res => {
+        console.log(res);
+
+      }, err => {
+        console.log('that is no good buddy....');
+        console.log(err);
+      }
+    )
   }
 
 }
