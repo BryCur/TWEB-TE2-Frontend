f3f523ad6bd0739cfe40582af0c85c5c97004b43
Bryan <bryan.curchod@gmail.com>
2019-01-10 16:13:33 +0100

    Correction for upcomming films and filtering by genre

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ Author : Bryan Curchod
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.2.7.
 
 Unfortunately I couldn't finish the project, here's a list of what's missing : 
-- Film filtering in the upcomming page
 - Favorite persistence when returning on the popular page
 - **D E P L O Y M E N T**
 - Pagination (get the nexts pages)
--- src/app/upcomming-page/upcomming-page.component.ts
+++ src/app/upcomming-page/upcomming-page.component.ts
@@ -16,34 +16,57 @@ export class UpcommingPageComponent implements OnInit {
   totalPage: number;
   ok: boolean;
   tabFilm: FilmDTO[] = [];
-  favFilms: FilmDTO[];
+  favFilms: FilmDTO[] = [];
+  favGenres: Set<number> = new Set<number>();
 
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    // favorite retrieving 
     this.favFilms = JSON.parse(sessionStorage.getItem('favorites'));
+
+    if (this.favFilms) {
+      for (let i = 0; i < this.favFilms.length; ++i) {
+        this.favFilms[i].genre_ids.forEach(id => this.favGenres.add(id));
+      }
+    }
+
+    console.log(this.favGenres);
 
     this.executeRequest()
 
   }
 
   /**
-   * Execute the get request to gather the popular films
+   * Execute the get request to gather the upcomming films
    */
-  executeRequest(): void {
+  async executeRequest() {
     // TODO affiner la recherche
-    const requestURL = environment.API_URL + "3/movie/upcoming?page=" + this.currentPage + "&api_key=" + environment.API_KEY;
-    // lancement de la requête
-    this.http.get(requestURL).toPromise().then(
-      (res: reqResponse) => {
-        this.tabFilm = res.results;
-        this.totalPage = res.total_pages;
-      }, err => {
-        console.log('that is no good buddy....');
-        console.log(err);
-      }
-    )
+    for (let i = 1; i <= 5; ++i) {
+      const requestURL = environment.API_URL + "3/movie/upcoming?page=" + i + "&api_key=" + environment.API_KEY;
+      // lancement de la requête
+      await this.http.get(requestURL).toPromise().then(
+        (res: reqResponse) => {
+          res.results.forEach(f => this.tabFilm.push(f));
+          this.totalPage = res.total_pages;
+        }, err => {
+          console.log('that is no good buddy....');
+          console.log(err);
+        }
+      )
+    }
+
+    if (this.favGenres.size > 0) {
+      this.tabFilm = this.tabFilm.filter(elem => {
+        for (let i = 0; i < elem.genre_ids.length; ++i) {
+          if (this.favGenres.has(elem.genre_ids[i])) {
+            return true;
+          }
+        }
+        return false;
+      });
+    }
   }
 
 }
